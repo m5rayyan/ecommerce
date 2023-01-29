778794de8d90191f7a785faed52977e9e9b52eb4
MRayyan <eng.m.rayyan@gmail.com>
2023-01-29 10:29:34 +0200

    create ecommerce app

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      {/* <Home /> */}
-      <StoreList />
+      <Home />
+      {/* <StoreList /> */}
     </div>
   );
 }
